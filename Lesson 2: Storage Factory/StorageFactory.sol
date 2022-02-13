bdb02b4dd29fc745da96970c1c634d0f6c9f5604
ryan5481 <ryan5481@me.com>
2022-02-14 00:32:37 +0800

    added comments

--- Lesson 2: Storage Factory/StorageFactory.sol
+++ Lesson 2: Storage Factory/StorageFactory.sol
@@ -7,7 +7,11 @@ import "./SimpleStorage.sol";
 contract storageFactory {
     SimpleStorage[] public simpleStorageArray;
 
+    
+    //generate a contract of simple storage type 
     function createSimpleStorageContract() public {
+        //create a variable of type SimpeStorage called simpleStorage, 
+        //which is going to be a new SimoleStorage contract(takes no input parameters)
         SimpleStorage simpleStorage = new SimpleStorage();  
         simpleStorageArray.push(simpleStorage);
     }
