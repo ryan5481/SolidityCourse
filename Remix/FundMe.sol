8fd41f770ff35ebf2f40925518a4323d3d141298
ryan5481 <ryan5481@me.com>
2022-02-22 23:36:07 +0800

    Update FundMe.sol
    Completed the code, added withdraw function, owner constructor and onlyOwner modifier, and added for loop iteration to reset the value in the address after withdrawl is complete.

--- Remix/FundMe.sol
+++ Remix/FundMe.sol
@@ -1,32 +1,87 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT;
 pragma solidity >=0.6.6 <0.9.0;
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  // getRoundData and latestRoundData should both raise "No data present"
+  // if they do not have data to report, instead of returning unset values
+  // which could be misinterpreted as actual reported values.
+  function getRoundData(uint80 _roundId)
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
+
+  function latestRoundData()
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
+}
 contract FundMe{
 
-    mapping (address => uint256) public addrToAmountFunded;
-    function fund() public payable {
-        addrToAmountFunded[msg.sender] += msg.value;
-        uint256 minimumUSD = 50 * 10 ** 18;
-        require (getConversionRate(msg.value) >= minimumUSD, "ETH lower than required.");
-        //; addr of msgsender =+ value
-        // keeps track of all the addr thatsends value; someone will send 'value' to the addr 
+mapping (address => uint256) public addrToAmountFunded;
+
+    address public owner;
+    address[] public funders;
+
+    constructor() {
+        owner = msg.sender;
     }
+
+    modifier onlyOwner {
+        require( msg.sender == owner);
+        _;
+    }
+
+    function fund() public payable{
+        uint256 minimumUSD = 50 * 10 ** 18;
+        require ( getConversionRate(msg.value) >= minimumUSD, "fund too low!");
+        addrToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+
     function getVersion() public view returns(uint256){
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        return  priceFeed.version();
+        return priceFeed.version();
     }
 
     function getPrice() public view returns(uint256){
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        (,int256 answer,,,)= priceFeed.latestRoundData();
-         return uint256 (answer * 10000000000) ;
-         //2724.47706479
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+        return uint256(answer * 10000000000);
+        
     }
-    // convert whatever ETH value sent into USD
-    function getConversionRate(uint256 ethAmount) public view returns (uint256){
+
+    function getConversionRate(uint256 ethAmount) public view returns(uint256){
         uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
-        return ethAmountInUsd;
+        uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1000000000000000000;
+        return ethAmountInUSD;
     }
+
+    function withdraw() payable onlyOwner public{
+        msg.sender.transfer(address(this).balance);
+
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addrToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+    }
+
 }
